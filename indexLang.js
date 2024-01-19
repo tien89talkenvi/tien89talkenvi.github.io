f0556e74030695dc1708dd0470c118491387ad2b
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2024-01-19 22:10:25 +0700

    Add files via upload

---
+++ indexLang.js
@@ -0,0 +1,12 @@
+var langs = //list cac list
+[['Deutsch',       ['de-DE']],
+ ['English',       ['en-US']],
+ ['Français',      ['fr-FR']],
+ ['Italiano',      ['it-IT']],
+ ['Nederlands',    ['nl-NL']],
+ ['Română',        ['ro-RO']],
+ ['Rusian',        ['ru-RU']],
+ ['Korean',        ['ko-KR']],
+ ['Taiwan',        ['zh-TW']],
+ ['Japanese',      ['ja-JP']],
+ ['Vietnamese',    ['vi-VN']]];    
